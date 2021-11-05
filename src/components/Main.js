44c5715a470d4f5c37f8b10114c366e179902d50
Wojciech Fydrych <wfydrych@joinpiggy.com>
2021-11-05 08:43:51 +0100

    Text added

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,24 +8,27 @@ export default function Main() {
     <>
         <div className='main'></div>
         <div className='introduce'>
-            <div className='introduce__title'>LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISCING</div>
+            <div className='introduce__title'>Autopflege, Autoreinigung, Keramikversieglung der Spitzenklasse!</div>
             <div className='introduce__advertbox'>
                 <div className='introduce__advertbox__advert'>
                     <img alt='pic1' src={ad1} />
                     <div className='introduce__advertbox__advert__text'>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nibh augue, suscipit a, scelerisque sed, lacinia in, mi.
+                        <div><h2>Autopflege</h2></div>
+Ihr Fahrzeug verdient die beste Pflege mit hochwertigen Produkten. Wir stehen für professionelle Dienstleistung in der Autopflege und Aufbereitung bis zur Keramikversieglung.
                     </div>
                 </div>
                 <div className='introduce__advertbox__advert'>
                     <img alt='pic1' src={ad2} />
                     <div className='introduce__advertbox__advert__text'>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nibh augue, suscipit a, scelerisque sed, lacinia in, mi.
+                        <div><h2>Keramikversieglung</h2></div>
+                    Die Keramikversieglung macht das Auto widerstandsfähig gegen äussere Einflüsse und vereinfacht die Reinigung. Je nach Witterung hält die Keramikversieglung für ca. 30 bis 36 Monate.
                     </div>
                 </div>
                 <div className='introduce__advertbox__advert'>
                     <img alt='pic1' src={ad3} />
                     <div className='introduce__advertbox__advert__text'>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nibh augue, suscipit a, scelerisque sed, lacinia in, mi.
+                        <div><h2>Innenraum Desinfektion</h2></div>
+                    Schützen Sie sich und Ihre Beifahrer vor Bakterien und Viren. Die Innenraum Desinfektion beinhaltet: – Gesamter Inenraumdesinfektion – Infektionsschutz – Virenvernichtung.
                     </div>
                 </div>
             </div>
@@ -33,45 +36,42 @@ export default function Main() {
         <div className='offer'>
             <div className='offer__backgr'>
                 <div className='offer__backgr__title'>
-                    LOREM IPSUM DOLOR SIT
+                    VON DER LEIDENSCHAFT ZUM BERUF
                 </div>
                 <div className='offer__backgr__description'>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nibh augue, suscipit a, 
-                    scelerisque sed, lacinia in, mi. Cras vel lorem. Etiam pellentesque aliquet tellus. 
-                    Phasellus pharetra nulla ac diam.
+                    Wie so oft begann alles als Hobby. Ich fing an eigenen Autos mit Waschen und Polieren an, machte anfängliche Fehler, aus denen ich lernte. Und so entwickelte sich aus diesem Hobby eine Leidenschaft.<br /><br />
+                    Was einst mit einer billigen Maschine begann, steigerte sich immer weiter zu hochpreisigen Produkten der Premiumklasse. Von den besten Wachsen und Versiegelungen bis hin zu mehreren Poliermaschinen ist nun alles im Einsatz. Wir haben es uns zum Ziel gesetzt jeden Lack auf besondere Art und Weise zum Glänzen zu bringen. Ohne, dass man gleich die Nummer eines Lackierers wählen muss.<br /><br />
+                    Unsere Mission ist es, jedes Fahrzeug möglichst nahe an den Neuwagenzustand zu bringen und das mit qualitativ hochwertigen Produkten. Wir arbeiten an jedem Fahrzeug mit Leidenschaft, um das perfekte Gesamtbild zu erzeugen. Dazu arbeiten wir mit Wachsen oder Versiegelungen, die auf das Fahrzeug perfekt abgestimmt sind.
                 </div>
             </div>
             <div className='offer__textbox'>
                 <div className='offer__textbox__title'>
-                    LOREM IPSUM DOLOR SIT
+                    
                 </div>
                 <div className='offer__textbox__container'>
                     <div className='offer__textbox__container__box'>
                         <div className='offer__textbox__container__box__rectangle'></div>
                         <div className='offer__textbox__container__box__text'>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                            Proin nibh augue, suscipit a, scelerisque sed, lacinia in, mi.
+                        Shinefactory steht dafür, dass ihr Fahrzeug – egal ob Kleinwagen, Sportwagen oder aber auch Flugzeug – wieder im richtigen Licht steht und sich Andere nach Ihnen und Ihrem Fahrzeug umgucken. Dafür sorgen wir, Jimmy Strzecha und kim Tyszlak, höchstpersönlich.
                         </div>
                     </div>
                     <div className='offer__textbox__container__box'>
                         <div className='offer__textbox__container__box__rectangle'></div>
                         <div className='offer__textbox__container__box__text'>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                            Proin nibh augue, suscipit a, scelerisque sed, lacinia in, mi.
+                        Vereinbaren Sie gerne einen unverbindlichen Beratungstermin. Gerne werde ich Sie beraten und ein auf sie zugeschnittenes Angebot ganz nach Ihren Vorstellungen und Ihrem Budget erstellen.
                         </div>
                     </div>
                     <div className='offer__textbox__container__box'>
                         <div className='offer__textbox__container__box__rectangle'></div>
                         <div className='offer__textbox__container__box__text'>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                            Proin nibh augue, suscipit a, scelerisque sed, lacinia in, mi.
+                        Wir achten bei jeder Dienstleistung auf kompromisslose Qualität und sind jederzeit für Sie da, wenn Sie eine individuelle Beratung wünschen. Wir sind dein kompetenter Partner, wenn es um die Pflege deines Autos geht.
                         </div>
                     </div>
                 </div>
             </div>
         </div>
         <div className='bottomtext'>
-            LOREM IPSUM DOLOR SIT
+            CAR SHINE FACTORY
         </div>
     </>
   )
